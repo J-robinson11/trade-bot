009563c6ecad9030454d67a83797ed4c137d165d
J-robinson11 <104178757+J-robinson11@users.noreply.github.com>
2023-12-21 03:09:02 -0600

    fixing git issue

---
+++ code/config.py
@@ -0,0 +1,5 @@
+ALPACA_API_KEY = 'AKLWPKASYVLE3CNX2QBQ'
+ALPACA_SECRET_KEY = '0vkcOr5IgXaVMT0AoJkaAd7lTxsoVCy2V2qK4T40'
+ALPACA_PAPER_URL = 'https://paper-api.alpaca.markets'
+ALPACA_LIVE_URL = 'https://api.alpaca.markets'
+# Add other configuration settings as neede
